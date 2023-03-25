1ffe0ca1447df905be4b9b45452acc12262d6c4f
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2023-03-25 15:09:01 +0300

    add image app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
+<script setup>
+  import SideBar from './components/SideBar.vue'
+  import ImageGallery from "@/components/ImageGallery.vue";
+  import {ref} from "vue";
+
+  const isWithColor = ref(true)
+
+  const onSelectOption = (isColor) => {
+    isWithColor.value = isColor
+  }
+</script>
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <SideBar @selectOption="onSelectOption"/>
+    <v-main>
+      <ImageGallery :isWithColor="isWithColor"/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+  },
+  data: () => ({
+    //
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
